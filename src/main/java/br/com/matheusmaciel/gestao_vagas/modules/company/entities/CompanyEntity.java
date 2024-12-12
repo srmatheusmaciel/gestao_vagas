20d3e2e9ae10ccc66badadf6b30123ec794a52a0
srmatheusmaciel <srmatheusmaciel@hotmail.com>
2024-12-12 07:44:39 -0300

    Spring Security Configuration Implementation
    Signed-off-by: srmatheusmaciel <srmatheusmaciel@hotmail.com>

---
+++ src/main/java/br/com/matheusmaciel/gestao_vagas/modules/company/entities/CompanyEntity.java
@@ -0,0 +1,41 @@
+package br.com.matheusmaciel.gestao_vagas.modules.company.entities;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Entity(name = "company")
+@Data
+public class CompanyEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+  private String name;
+  @Email(message = "O campo [email] deve conter um email válido")
+  private String email;
+
+  @NotBlank(message = "O campo [username] deve ser preenchido")
+  @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaços")
+  private String username;
+
+  @Length(min = 8, max = 100, message = "A senha deve conter no mínimo 8 caracteres")
+  private String password;
+  private String description;
+  private String website;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+
+}
---
+++ src/main/java/br/com/matheusmaciel/gestao_vagas/modules/company/entities/JobEntity.java
@@ -0,0 +1,43 @@
+package br.com.matheusmaciel.gestao_vagas.modules.company.entities;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+
+@Entity(name = "job")
+@Data
+public class JobEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+  private String description;
+  private String benefits;
+
+  @NotBlank(message = "O campo [level] deve ser preenchido")
+  private String level;
+
+  @ManyToOne
+  @JoinColumn(name = "company_id")
+  private CompanyEntity companyEntity;
+
+  @Column(name = "company_id", insertable = false, updatable = false)
+  private UUID companyId;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+
+
+}
