4858535d9639032072815e2e5dc89d2bce578a10
srmatheusmaciel <srmatheusmaciel@hotmail.com>
2024-12-24 08:52:58 -0300

    Created UseCase: a method ListAllJobsByFilter
    Signed-off-by: srmatheusmaciel <srmatheusmaciel@hotmail.com>

--- src/main/java/br/com/matheusmaciel/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/matheusmaciel/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,5 +1,6 @@
 package br.com.matheusmaciel.gestao_vagas.modules.candidate.controllers;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,13 +10,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.matheusmaciel.gestao_vagas.exceptions.UserFoundException;
 import br.com.matheusmaciel.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.matheusmaciel.gestao_vagas.modules.candidate.CandidateRepository;
 import br.com.matheusmaciel.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
+import br.com.matheusmaciel.gestao_vagas.modules.candidate.useCases.ListAllJobsByFilterUseCase;
 import br.com.matheusmaciel.gestao_vagas.modules.candidate.useCases.ProfileCandidateUseCase;
+import br.com.matheusmaciel.gestao_vagas.modules.company.entities.JobEntity;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
@@ -28,6 +32,9 @@ public class CandidateController {
 
   @Autowired
   private ProfileCandidateUseCase profileCandidateUseCase;
+
+   @Autowired
+  private ListAllJobsByFilterUseCase listAllJobsByFilterUseCase;
 
   @PostMapping("/")
   public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
@@ -51,5 +58,11 @@ public class CandidateController {
     
     }
 
+  }
+
+   @GetMapping("/job")
+  @PreAuthorize("hasRole('CANDIDATE')")
+  public List<JobEntity> findJobByFilter(@RequestParam String filter) {
+    return this.listAllJobsByFilterUseCase.execute(filter);
   }
 }
