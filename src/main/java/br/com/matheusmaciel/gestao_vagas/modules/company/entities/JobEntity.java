9205a72a8b7c460627bbefe0f1b5fa351be9b6b7
srmatheusmaciel <srmatheusmaciel@hotmail.com>
2024-12-26 17:20:50 -0300

    Insert auth on swagger
    Signed-off-by: srmatheusmaciel <srmatheusmaciel@hotmail.com>

--- src/main/java/br/com/matheusmaciel/gestao_vagas/modules/company/entities/JobEntity.java
+++ src/main/java/br/com/matheusmaciel/gestao_vagas/modules/company/entities/JobEntity.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -29,10 +30,14 @@ public class JobEntity {
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
+
+  @Schema(example = "Vaga para design")
   private String description;
+  @Schema(example = "GymPass, Plano de saúde")
   private String benefits;
 
   @NotBlank(message = "O campo [level] deve ser preenchido")
+  @Schema(example = "SENIOR")
   private String level;
 
   @ManyToOne
